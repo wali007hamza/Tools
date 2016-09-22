29e6ceb3d9131af7b6ae1bd70af1873067a3a981
syhamza <syhamza@microsoft.com>
2016-09-21 19:26:21 -0700

    More Additions

--- AliasSetter.ps1
+++ AliasSetter.ps1
@@ -5,64 +5,73 @@ $env:PATH += ";C:\MyRepo\Tools"
 #Development Aliases
 
 function OpenOls{
-	devenv $Env:srcroot\ols\ols.sln
+    devenv $Env:srcroot\ols\ols.sln
 }
 set-alias dev_ols OpenOls
 
 function OpenSvcDef{
-	devenv $Env:srcroot\osisvcdef\ols\src\servicedefinitions\ols\ols.sln
+    devenv $Env:srcroot\osisvcdef\ols\src\servicedefinitions\ols\ols.sln
 }
 set-alias dev_svcdef OpenSvcDef
 
 function SvcDefDir{
-	cd $Env:srcroot\osisvcdef\ols\src\servicedefinitions\ols\
+    cd $Env:srcroot\osisvcdef\ols\src\servicedefinitions\ols\
 }
 set-alias svcdef_dir SvcDefDir
 
+function SvcDefRoot{
+    cd $Env:srcroot\osisvcdef\ols
+}
+set-alias svcdef_root SvcDefRoot
+
+function OlsTarget{
+    cd $Env:targetroot\x64\debug\ols\x-none
+}
+
 function OlsEnvironmentsDir{
-	cd $Env:targetroot\x64\debug\olssetup\en-us\setup\deployment\environments
+    cd $Env:targetroot\x64\debug\olssetup\en-us\setup\deployment\environments
 }
 set-alias olsenv_dir OlsEnvironmentsDir
 
 function EnvironmentsDir($project){
-	cd $Env:targetroot\x64\debug\"osiedgen_$($project)"\x-none\EnvironmentDescriptionFiles
+    cd $Env:targetroot\x64\debug\"osiedgen_$($project)"\x-none\EnvironmentDescriptionFiles
 }
 
 function ToRoot{
-	cd $env:srcroot
+    cd $env:srcroot
 }
 set-alias root ToRoot
 
 function OpenAliasSetter{
-	subl C:\MyRepo\Tools\AliasSetter.ps1
+    subl C:\MyRepo\Tools\AliasSetter.ps1
 }
 set-alias open_aliasSetter OpenAliasSetter
 
 set-alias run_aliasSetter C:\MyRepo\Tools\AliasSetter.ps1
 
 function InitEnv{
-	C:\MyRepo\Tools\PowerShellEnvironmentSetter.ps1
+    C:\MyRepo\Tools\PowerShellEnvironmentSetter.ps1
 }
 set-alias init_env InitEnv
 
 function OpenEnvSetter{
-	subl C:\MyRepo\Tools\PowerShellEnvironmentSetter.ps1
+    subl C:\MyRepo\Tools\PowerShellEnvironmentSetter.ps1
 }
 set-alias open_envsetter OpenEnvSetter
 
 function SvcDef([string]$project){
-	cd $Env:srcroot\osisvcdef\$project\src\servicedefinitions\$project\
+    cd $Env:srcroot\osisvcdef\$project\src\servicedefinitions\$project\
 }
 
 set-alias dev devenv
 
 function OpenResearch{
-	. $Env:otools\bin\research
+    . $Env:otools\bin\research
 }
 set-alias research OpenResearch
 
 function ToMyTools{
-	cd C:\MyRepo\Tools
+    cd C:\MyRepo\Tools
 }
 set-alias mytools ToMyTools
 
